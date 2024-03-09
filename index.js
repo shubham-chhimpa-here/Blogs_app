199041e416bf8015c1916fff02066e9ff49a1b39
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-03-09 11:14:16 +0530

    submit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const { userRouter } = require('./router/user.router');
 require('dotenv').config()
 
 const app = express()
 
 const PORT = process.env.PORT;
 
-
+app.use('/user' , userRouter)
 app.get('/', (req, res) => {
     res.send({msg: 'home page'})
 })
