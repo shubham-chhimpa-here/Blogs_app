25a47f4d9396baf53f54776fd90c97f70d639820
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-04-11 07:58:53 +0530

    submit

--- router/user.router.js
+++ router/user.router.js
@@ -7,11 +7,29 @@ userRouter.get('/', async (req, res) => {
     res.send({ msg: 'user router', data })
 })
 
-userRouter.post('/add', async (req, res) => {
+userRouter.post('/register', async (req, res) => {
     const data = new UserModel(req.body)
     await data.save()
 
     res.send({ data })
 })
+
+userRouter.post('/login', async (req, res) => {
+    const { name, email, password } = req.body
+    console.log(name, email, password)
+    const user = await UserModel.findOne({ email })
+    if (user) {
+        if (user.password === password) {
+            res.send({ status: true, token: 'token' })
+        }
+        else {
+            res.send({ msg: 'wrong credential' })
+        }
+    }
+    else {
+        res.send({ msg: 'user not found' })
+    }
+})
+
 
 module.exports = { userRouter }
