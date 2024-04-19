b69c6bd55e5462c3cde144fd8ece6f8a1e01b166
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-04-19 15:07:46 +0530

    submit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(cors())
-app.use(logger)
+// app.use(logger)
 app.use('/user', userRouter)
 app.use('/blog', blogRouter)
 
