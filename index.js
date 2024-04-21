1420276d4991e28257a780316882e7bd85cc0179
Shubham Chhimpa <145216988+shubham-chhimpa-here@users.noreply.github.com>
2024-04-21 10:30:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors())
 // app.use(logger)
 app.use((req, res, next) => {
     const origin = req.headers.origin;
-    // console.log(origin)
+    console.log(origin, allowedOrigins)
+    
     if (allowedOrigins === origin) {
         // res.setHeader('Access-Control-Allow-Origin', origin);
     next();
